c9581f47d6f5dd51cd85bb5a15e72ad281c45c1c
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-11-02 20:49:37 +0600

    add in the Adina.js closses Adina

--- src/components/Adina/Adina.js
+++ src/components/Adina/Adina.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Adina.module.css'
 
 function Adina() {
-    return ( < div >
+    return ( < div className = { classes.Adina } >
         <
         div className = { classes.hello1 } > < span > У Адины вы сможете < /span> <p> сделать идеальный качественный ЛАМИНИРОВАНИЯ РЕСНИЦ, а также уход за вашими ресницами принимает домашнем уюте </p > < /div > <
         /div>
