2ec9f1df68ab250076c5f749f66f6b7ba4069ff9
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-11-02 21:19:36 +0600

    add new img icons

--- src/components/Adina/Adina.js
+++ src/components/Adina/Adina.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './Adina.module.css'
+import img13 from '../../Imges/img1.jpg'
 
 function Adina() {
     return ( < div className = { classes.Adina } >
@@ -7,6 +8,11 @@ function Adina() {
         div className = { classes.hello1 } > < span className = { classes.span1 } > У Адины вы сможете < /span> <p> Cделать идеальный качественный <
         span className = { classes.Glav } > ЛАМИНИРОВАНИЯ РЕСНИЦ < /span> , 
         а также уход за вашими ресницами принимает домашнем уюте < /p></div >
+
+
+        <
+        img src = { img13 }
+        alt = "helo" / >
         <
         /div>
     )
