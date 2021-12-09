4d54df0245090db050b9b96c53207c96a5327617
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-12-09 16:18:06 +0600

    add bootstrapt and other program tegs

--- src/components/Zapis/Zapis.js
+++ src/components/Zapis/Zapis.js
@@ -1,16 +1,35 @@
-import classes from './Zapis.module.css'
 import React from 'react'
+import emailjs from 'emailjs-com'
+import classes from './Zapis.module.css'
 
 function Zapis() {
-    return ( <
-        div >
+    function sendEmail(e) {
+        e.preventDefault()
+
+        emailjs.sendForm('service_8sd7yz7', 'template_v5kfyz1', e.target, 'user_0Pk5tbcbJAxlB1kZJgXJk').then(res => {
+            console.log(res);
+        }).catch(err => { console.log(err); })
+    }
+    return ( < div className = { classes.container } >
+
         <
-        h1 > Записаться < /h1> <
-        div > < div className = { classes.space } > < label > Ф.И.О < /label><input type="text"/ > < /div> <
-        div className = { classes.space } > < label > Email < /label><input type="text"/ > < /div> <
-        div className = { classes.space } > < label > Сообщение < /label><input type="text"/ > < /div></div >
+        form className = { classes.Zapis }
+        onSubmit = { sendEmail } > < label > Name < /label><input name="lastname" type="text" placeholder="name"/ >
         <
-        button > Отправит < /button> <
+        label > Email < /label><input name="email" type="email" placeholder="email"/ >
+        <
+        label > message < /label> <
+        textarea name = "ms"
+        type = "text"
+        placeholder = "message" / >
+        <
+        label > Tel: < /label> <
+        input type = "tel"
+        name = "tel"
+        placeholder = "Tel-number" / >
+        <
+        input type = "submit"
+        value = "Send" / > < /form> <
         /div>
     )
 }
